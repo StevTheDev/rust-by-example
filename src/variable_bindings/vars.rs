698c49ffc6baf380b3bff446a558432a632b0136
Steven Fitzpatrick <stevthedev@gmail.com>
2020-06-19 01:37:55 -0500

    Variable Bindings

---
+++ src/variable_bindings/vars.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    // The compiler warns about unused variable bindings; these warnings can
+    // be silenced by prefixing the variable name with an underscore
+    let _unused_variable = 3u32;
+
+    // Prefix unused variables with an underscore to suppress the warning
+    let _noisy_unused_variable = 2u32;
+    
+    // To change a variable it must be declared mutable
+    let mut mutable_binding = 1;
+    println!("Before mutation: {}", mutable_binding);
+    mutable_binding += 1;
+    println!("After mutation: {}", mutable_binding);
+
+}
